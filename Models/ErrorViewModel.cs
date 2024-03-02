15dece2a9a9976a5772d089b5a29a1f8fe1e9fd1
Admin_Venu <Admin_Venu@VENUPC>
2024-03-02 21:58:39 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotnetCoreApp1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
